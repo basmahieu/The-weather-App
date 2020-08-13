5ea790136751f043c345959f3a826ec199d64b74
Bas Mahieu <bas.mahieu@gmail.com>
2020-08-13 11:54:52 +0200

    adding drop it like its hot

--- js/app.js
+++ js/app.js
@@ -4,9 +4,21 @@ let name = document.querySelector(".name");
 let temp = document.querySelector(".temp");
 let desc = document.querySelector(".desc");
 let date = document.querySelector(".date");
+let icon = document.querySelector(".icon img");
+
+// Don't Display cards
+document.querySelector(".card").style.display = "none";
+
+// Get icon
+// const iconSrc = data["icon"];
+// const icons = `http://openweathermap.org/img/wn/${iconSrc}.png`;
+// icon.setAttribute("src", icons);
 
 // Get City & weather information
+// display Card on click
 document.querySelector("#run").onclick = () => {
+  document.querySelector(".card").style.display = "block";
+
   const getLocalWeather = async (city) => {
     const base = "https://api.openweathermap.org/data/2.5/weather?";
     const query = `&q=${city}&units=metric&appid=${key}`;
@@ -14,16 +26,27 @@ document.querySelector("#run").onclick = () => {
     const response = await fetch(base + query);
     const data = await response.json();
     return data;
+    // console.log(data);
   };
   getLocalWeather(inputValue.value)
     .then((data) => {
       let nameValue = data["name"];
       let tempValue = data["main"]["temp"];
       let descValue = data["weather"][0]["description"];
+      let test = data["weather"][0]["icon"];
+      let iconSrc = `img/icons/${test}.png`;
+      icon.setAttribute("src", iconSrc);
 
       name.innerHTML = nameValue;
-      temp.innerHTML = Math.floor(tempValue);
+      temp.innerHTML = Math.floor(tempValue) + "°";
       desc.innerHTML = descValue;
     })
-    .catch((err) => alert("there seems to be an issue, we're working on it."));
+
+    .catch((err) =>
+      console.log("there seems to be an issue, we're working on it.")
+    );
 };
+
+// let iconValue = data["icon"];
+
+// icon.innerHTML = `http://openweathermap.org/img/wn/${iconValue}.png`;
